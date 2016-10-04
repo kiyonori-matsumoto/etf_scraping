a2c39c60b0ab5935d10cda7e2c2e6433682450b6
kiyonori <kiyonori.matsumoto@gmail.com>
2016-10-05 01:09:19 +0900

    change primary_key of Issues to issue_code

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,6 @@
 class Issue < ActiveRecord::Base
-  has_many :dailies
+  has_many :dailies, foreign_key: :issue_code
   validates :company, presence: true, inclusion: { in: %w(nikko blackrock daiwa) }
 
-  # self.primary_key = :code
+  self.primary_key = :code
 end
