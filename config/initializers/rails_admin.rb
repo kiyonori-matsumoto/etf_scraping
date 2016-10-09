b758a93b499a3fc5e2dba6aab7ca10ed42f6db31
kiyonori <kiyonori.matsumoto@gmail.com>
2016-10-09 20:44:03 +0900

    change admin value to default

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -39,3 +39,22 @@ RailsAdmin.config do |config|
     # history_show
   end
 end
+
+module RailsAdmin
+  module Config
+    module Fields
+      module Types
+        class Datetime < RailsAdmin::Config::Fields::Base
+          register_instance_option :date_format do
+            :default
+          end
+        end
+        class Date < RailsAdmin::Config::Fields::Types::Datetime
+          register_instance_option :date_format do
+            :default
+          end
+        end
+      end
+    end
+  end
+end
