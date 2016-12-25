f31a8098ac15585be31dea17275a80776d13a970
kiyonori <kiyonori.matsumoto@gmail.com>
2016-12-26 07:02:31 +0900

    move unit helper to ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def unit
+    {units: {hundred: "百",:thousand => "千", :million => "百万", :billion => "十億", :trillion => "兆", :quadrillion => "千兆"}, precision: 5}
+  end
 end
